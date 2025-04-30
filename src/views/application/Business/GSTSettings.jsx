47299c6b9ca6257fd1f707a801224f3e04b79177
krishnakannekanti <krishnasai.kannekanti@financebox.in>
2025-04-30 20:53:48 +0530

    Adding GST details

--- src/views/application/Business/GSTSettings.jsx
+++ src/views/application/Business/GSTSettings.jsx
@@ -15,7 +15,6 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
-  Grid,
   TextField,
   Switch,
   FormControlLabel,
@@ -30,46 +29,50 @@ import {
   Stack,
   Tooltip
 } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CloseIcon from '@mui/icons-material/Close';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
+import { useSelector } from 'store';
+import Factory from 'utils/Factory';
 
 const initialGSTs = [
   {
-    gst_number: '27AAEPM1234C1Z5',
+    gstin: '27AAEPM1234C1Z5',
     trade_name: 'Acme Corp',
-    branch: 'Mumbai',
+    branch_name: 'Mumbai',
     state: 'Maharashtra',
     type: 'Regular',
-    export_sez: true
+    is_export_sez: true
   },
   {
-    gst_number: '29AAEPM5678B1Z2',
+    gstin: '29AAEPM5678B1Z2',
     trade_name: 'Beta Pvt Ltd',
-    branch: 'Bangalore',
+    branch_name: 'Bangalore',
     state: 'Karnataka',
     type: 'Composition',
-    export_sez: false
+    is_export_sez: false
   }
 ];
 
 const validationSchema = Yup.object({
-  gst_number: Yup.string().required('GST Number is required'),
+  gstin: Yup.string().required('GST Number is required'),
   legal_name: Yup.string().required('Legal Name is required'),
   trade_name: Yup.string().required('Trade Name is required'),
-  branch: Yup.string().required('Branch/Vertical is required'),
-  username: Yup.string().required('Username in GST is required'),
-  password: Yup.string().required('Password is required'),
-  signatory_pan: Yup.string().required('Authorized Signatory PAN is required'),
+  branch_name: Yup.string().required('Branch/Vertical is required'),
+  gst_username: Yup.string().required('Username in GST is required'),
+  gst_password: Yup.string().required('Password is required'),
+  authorized_signatory_pan: Yup.string().required('Authorized Signatory PAN is required'),
   address: Yup.string().required('Address is required'),
   state: Yup.string().required('State is required'),
-  zip: Yup.string().required('Zip Code is required'),
-  composition: Yup.boolean(),
-  composition_perc: Yup.string(),
-  export_sez: Yup.boolean(),
+  pinCode: Yup.string().required('Zip Code is required'),
+  is_composition_scheme: Yup.boolean(),
+  composition_scheme_percent: Yup.string(),
+  is_export_sez: Yup.boolean(),
   lut_reg_no: Yup.string(),
   dob: Yup.string(),
   financial_year: Yup.string()
@@ -94,6 +97,7 @@ const GSTSettings = () => {
   const [gstList, setGstList] = useState(initialGSTs);
   const [open, setOpen] = useState(false);
   const [editIndex, setEditIndex] = useState(null);
+  const user = useSelector((state) => state).accountReducer.user;
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
@@ -106,9 +110,9 @@ const GSTSettings = () => {
     setEditIndex(index);
     formik.setValues({
       ...gstList[index],
-      username: '',
-      password: '',
-      gst_certificate: null
+      gst_username: '',
+      gst_password: '',
+      gst_document: null
     });
     setOpen(true);
   };
@@ -121,37 +125,65 @@ const GSTSettings = () => {
 
   const formik = useFormik({
     initialValues: {
-      gst_number: '',
+      gstin: '',
       legal_name: '',
       trade_name: '',
-      branch: '',
-      username: '',
-      password: '',
-      signatory_pan: '',
-      gst_certificate: null,
+      branch_name: '',
+      gst_username: '',
+      gst_password: '',
+      authorized_signatory_pan: '',
+      gst_document: null,
       address: '',
       state: '',
-      zip: '',
-      composition: false,
-      composition_perc: '',
-      export_sez: false,
+      pinCode: '',
+      is_composition_scheme: false,
+      composition_scheme_percent: '',
+      is_export_sez: false,
       lut_reg_no: '',
       dob: '',
       financial_year: ''
     },
     validationSchema,
-    onSubmit: (values) => {
-      const data = { ...values };
-      if (editIndex !== null) {
-        const updated = [...gstList];
-        updated[editIndex] = data;
-        setGstList(updated);
-      } else {
-        setGstList([...gstList, data]);
+    onSubmit: async (values, { setSubmitting }) => {
+      try {
+        const formData = new FormData();
+        formData.append('business', user.active_context.business_id);
+        Object.keys(values).forEach((key) => {
+          if (key !== 'gst_document') {
+            formData.append(key, values[key]);
+          }
+        });
+
+        if (values.gst_document) {
+          formData.append('gst_document', values.gst_document);
+        }
+
+        const response = await Factory('post', '/business/gst-settings', formData, {});
+
+        if (response.res.status_cd === 0) {
+          console.log(response.res);
+        }
+
+        // if (response.data) {
+        //   if (editIndex !== null) {
+        //     const updated = [...gstList];
+        //     updated[editIndex] = response.data;
+        //     setGstList(updated);
+        //   } else {
+        //     setGstList([...gstList, response.data]);
+        //   }
+        //   handleClose();
+        // }
+      } catch (error) {
+        console.error('Error submitting GST details:', error);
+        // You might want to show an error notification here
+      } finally {
+        setSubmitting(false);
       }
-      handleClose();
     }
   });
+
+  console.log(formik.errors);
 
   return (
     <Box>
@@ -198,12 +230,12 @@ const GSTSettings = () => {
             <TableBody>
               {gstList.map((row, idx) => (
                 <TableRow key={idx} hover>
-                  <TableCell>{row.gst_number}</TableCell>
+                  <TableCell>{row.gstin}</TableCell>
                   <TableCell>{row.trade_name}</TableCell>
-                  <TableCell>{row.branch}</TableCell>
+                  <TableCell>{row.branch_name}</TableCell>
                   <TableCell>{row.state}</TableCell>
-                  <TableCell>{row.type || (row.composition ? 'Composition' : 'Regular')}</TableCell>
-                  <TableCell>{row.export_sez ? 'Yes' : 'No'}</TableCell>
+                  <TableCell>{row.type || (row.is_composition_scheme ? 'Composition' : 'Regular')}</TableCell>
+                  <TableCell>{row.is_export_sez ? 'Yes' : 'No'}</TableCell>
                   <TableCell align="right">
                     <Stack direction="row" spacing={1} justifyContent="flex-end">
                       <Tooltip title="View/Edit">
@@ -243,20 +275,20 @@ const GSTSettings = () => {
             {/* GST Details Group */}
             <Box mb={2}>
               <Grid container spacing={2}>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
-                    id="gst_number"
-                    name="gst_number"
+                    id="gstin"
+                    name="gstin"
                     label="GST Number"
-                    value={formik.values.gst_number}
+                    value={formik.values.gstin}
                     onChange={formik.handleChange}
-                    error={formik.touched.gst_number && Boolean(formik.errors.gst_number)}
-                    helperText={formik.touched.gst_number && formik.errors.gst_number}
+                    error={formik.touched.gstin && Boolean(formik.errors.gstin)}
+                    helperText={formik.touched.gstin && formik.errors.gstin}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
@@ -269,7 +301,7 @@ const GSTSettings = () => {
                     helperText={formik.touched.legal_name && formik.errors.legal_name}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
@@ -282,20 +314,20 @@ const GSTSettings = () => {
                     helperText={formik.touched.trade_name && formik.errors.trade_name}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
-                    id="branch"
-                    name="branch"
+                    id="branch_name"
+                    name="branch_name"
                     label="Branch/Vertical"
-                    value={formik.values.branch}
+                    value={formik.values.branch_name}
                     onChange={formik.handleChange}
-                    error={formik.touched.branch && Boolean(formik.errors.branch)}
-                    helperText={formik.touched.branch && formik.errors.branch}
+                    error={formik.touched.branch_name && Boolean(formik.errors.branch_name)}
+                    helperText={formik.touched.branch_name && formik.errors.branch_name}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
@@ -308,7 +340,7 @@ const GSTSettings = () => {
                     helperText={formik.touched.state && formik.errors.state}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
@@ -321,71 +353,71 @@ const GSTSettings = () => {
                     helperText={formik.touched.address && formik.errors.address}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
-                    id="zip"
-                    name="zip"
+                    id="pinCode"
+                    name="pinCode"
                     label="Zip Code"
-                    value={formik.values.zip}
+                    value={formik.values.pinCode}
                     onChange={formik.handleChange}
-                    error={formik.touched.zip && Boolean(formik.errors.zip)}
-                    helperText={formik.touched.zip && formik.errors.zip}
+                    error={formik.touched.pinCode && Boolean(formik.errors.pinCode)}
+                    helperText={formik.touched.pinCode && formik.errors.pinCode}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
-                    id="signatory_pan"
-                    name="signatory_pan"
+                    id="authorized_signatory_pan"
+                    name="authorized_signatory_pan"
                     label="Authorized Signatory PAN"
-                    value={formik.values.signatory_pan}
+                    value={formik.values.authorized_signatory_pan}
                     onChange={formik.handleChange}
-                    error={formik.touched.signatory_pan && Boolean(formik.errors.signatory_pan)}
-                    helperText={formik.touched.signatory_pan && formik.errors.signatory_pan}
+                    error={formik.touched.authorized_signatory_pan && Boolean(formik.errors.authorized_signatory_pan)}
+                    helperText={formik.touched.authorized_signatory_pan && formik.errors.authorized_signatory_pan}
                   />
                 </Grid>
 
-                <Grid item xs={12} sm={6} md={4}></Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}></Grid>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
-                    id="username"
-                    name="username"
-                    autoComplete="new-username"
+                    id="gst_username"
+                    name="gst_username"
+                    autoComplete="new-gst_username"
                     label="Username in GST"
-                    value={formik.values.username}
+                    value={formik.values.gst_username}
                     onChange={formik.handleChange}
-                    error={formik.touched.username && Boolean(formik.errors.username)}
-                    helperText={formik.touched.username && formik.errors.username}
+                    error={formik.touched.gst_username && Boolean(formik.errors.gst_username)}
+                    helperText={formik.touched.gst_username && formik.errors.gst_username}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
-                    id="password"
-                    name="password"
+                    id="gst_password"
+                    name="gst_password"
                     label="Password"
-                    autoComplete="new-password"
-                    type="password"
-                    value={formik.values.password}
+                    autoComplete="new-gst_password"
+                    type="gst_password"
+                    value={formik.values.gst_password}
                     onChange={formik.handleChange}
-                    error={formik.touched.password && Boolean(formik.errors.password)}
-                    helperText={formik.touched.password && formik.errors.password}
+                    error={formik.touched.gst_password && Boolean(formik.errors.gst_password)}
+                    helperText={formik.touched.gst_password && formik.errors.gst_password}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <Button variant="outlined" component="label" fullWidth size="small" sx={{ height: '40px' }}>
                     Upload GST Certificate
-                    <input type="file" hidden onChange={(e) => formik.setFieldValue('gst_certificate', e.currentTarget.files[0])} />
+                    <input type="file" hidden onChange={(e) => formik.setFieldValue('gst_document', e.currentTarget.files[0])} />
                   </Button>
-                  {formik.values.gst_certificate && (
+                  {formik.values.gst_document && (
                     <Typography variant="caption" sx={{ ml: 1 }}>
-                      {formik.values.gst_certificate.name}
+                      {formik.values.gst_document.name}
                     </Typography>
                   )}
                 </Grid>
@@ -398,37 +430,37 @@ const GSTSettings = () => {
                 Schemes & Exports
               </Typography>
               <Grid container spacing={2}>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <FormControlLabel
                     control={
                       <Switch
-                        checked={formik.values.composition}
-                        onChange={(e) => formik.setFieldValue('composition', e.target.checked)}
-                        name="composition"
+                        checked={formik.values.is_composition_scheme}
+                        onChange={(e) => formik.setFieldValue('is_composition_scheme', e.target.checked)}
+                        name="is_composition_scheme"
                       />
                     }
                     label="Are you Reg. under Composition Scheme?"
                   />
                 </Grid>
-                {formik.values.composition && (
-                  <Grid item xs={12} sm={6} md={4}>
+                {formik.values.is_composition_scheme && (
+                  <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                     <Typography variant="body2" sx={{ mb: 1 }}>
                       Composition Scheme %
                     </Typography>
-                    <RadioGroup row name="composition_perc" value={formik.values.composition_perc} onChange={formik.handleChange}>
+                    <RadioGroup row name="composition_scheme_percent" value={formik.values.composition_scheme_percent} onChange={formik.handleChange}>
                       {compositionPercOptions.map((perc) => (
                         <FormControlLabel key={perc} value={perc} control={<Radio size="small" />} label={perc} />
                       ))}
                     </RadioGroup>
                   </Grid>
                 )}
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <FormControlLabel
                     control={
                       <Switch
-                        checked={formik.values.export_sez}
-                        onChange={(e) => formik.setFieldValue('export_sez', e.target.checked)}
-                        name="export_sez"
+                        checked={formik.values.is_export_sez}
+                        onChange={(e) => formik.setFieldValue('is_export_sez', e.target.checked)}
+                        name="is_export_sez"
                       />
                     }
                     label="Is your business involved in export/supply to sez/deemed exports?"
@@ -443,7 +475,7 @@ const GSTSettings = () => {
                 LUT Details
               </Typography>
               <Grid container spacing={2}>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
@@ -456,7 +488,7 @@ const GSTSettings = () => {
                     helperText={formik.touched.lut_reg_no && formik.errors.lut_reg_no}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <TextField
                     fullWidth
                     size="small"
@@ -471,7 +503,7 @@ const GSTSettings = () => {
                     helperText={formik.touched.dob && formik.errors.dob}
                   />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4}>
+                <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                   <FormControl fullWidth size="small" error={formik.touched.financial_year && Boolean(formik.errors.financial_year)}>
                     <InputLabel>Financial Year</InputLabel>
                     <Select
