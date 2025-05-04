e6ee19e43fb7504a8932209f22f9818579f2de62
anandgarikapati1 <anand.g@financebox.in>
2025-05-04 17:34:37 +0530

    worked on services get api

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
-import path from 'path'; // ✅ Add this import
 
 export default defineConfig(({ mode }) => {
   // depending on your application, base can also be "/"
@@ -42,7 +41,6 @@ export default defineConfig(({ mode }) => {
         //   find: 'assets',
         //   replacement: path.join(process.cwd(), 'src/assets')
         // },
-        '@': path.resolve(__dirname, 'src'), // ✅ Add this line for '@/'
         '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
       }
     },
