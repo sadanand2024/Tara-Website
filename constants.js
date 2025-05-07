e311a9d06d26741db03799896d37dcc98cc1ebd8
KrishTaraFirst <krishna@tarafirst.com>
2025-05-07 21:54:12 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://dev-backend.tarafirst.com:8000';
-// export const BASE_URL = 'https://prod.tarafirst.com';
+// export const BASE_URL = 'http://dev-backend.tarafirst.com:8000';
+export const BASE_URL = 'https://prod.tarafirst.com';
 // export const BASE_URL = 'https://prod.tarafirst.com:8000';
 // export const BASE_URL = 'http://test-backend.tarafirst.com:8000';
 // export const BASE_URL = 'http://192.168.1.13:9000';
