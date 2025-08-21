87d49af4bc3859c19970281f0b2ec73b1460a395
anandgarikapti <anand@tarafirst.com>
2025-08-21 00:15:05 -0700

    worked on forgot snackbar

--- src/views/application/EmployeePortal/Attendance/WebSocketStatus.jsx
+++ src/views/application/EmployeePortal/Attendance/WebSocketStatus.jsx
@@ -2,42 +2,28 @@ import React from 'react';
 import useWebSocket from 'react-use-websocket';
 
 const WebSocketStatus = () => {
-
-  const socketState = useWebSocket('ws://dev-backend.tarafirst.com:8000/ws/attendance/2/', {
-    onOpen: (event) => {
-      console.log('🔗 WebSocket connected:', event);
-      // You can access event.target or other fields if needed
-    },
-    onMessage: (event) => {
-      console.log('📩 Message received:', event);
-
-      try {
-        const parsedData = JSON.parse(event.data);
-        console.log('✅ Parsed JSON:', parsedData);
-      } catch (err) {
-        console.warn('⚠️ Could not parse message as JSON:', event.data);
-      }
-    }
-    ,
-    onClose: (event) => {
-      console.log('❌ WebSocket disconnected:', {
-        code: event.code,
-        reason: event.reason,
-        wasClean: event.wasClean
-      });
-    },
-    onError: (e) => console.log('⚠️ WebSocket Error:', e),
-    shouldReconnect: () => true // Auto-reconnect on close
+  const socketUrl = 'ws://dev-backend.tarafirst.com:8000/ws/attendance/2/';
+  const {
+    sendMessage,
+    sendJsonMessage,
+    lastMessage,
+    lastJsonMessage,
+    readyState,
+    getWebSocket,
+  } = useWebSocket(socketUrl, {
+    onOpen: () => console.log('opened'),
+    onMessage: (event) => console.log('message', event),
+    onClose: () => console.log('closed'),
+    onError: (error) => console.log('error', error),
+    shouldReconnect: (closeEvent) => true,
   });
-  console.log(socketState)
 
   const connectionStatus = {
     0: '🟡 Connecting',
     1: '🟢 Open',
     2: '🟠 Closing',
     3: '🔴 Closed'
-  }[socketState.readyState];
-  // console.log(socketState.readyState);
+  }[readyState];
   return (
     <div style={{ padding: 20 }}>
       <h2>WebSocket Status</h2>
